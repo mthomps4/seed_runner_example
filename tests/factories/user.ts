a8d360142397ca70f0bb45ad36443bc56ded33d8
Bison Template <hi@echobind.com>
2021-08-26 09:49:58 -0400

    Initial commit from Bison Template!

---
+++ tests/factories/user.ts
@@ -0,0 +1,30 @@
+import Chance from 'chance';
+import { Role, Prisma } from '@prisma/client';
+
+import { buildPrismaIncludeFromAttrs, prisma } from '../helpers';
+import { hashPassword } from '../../services/auth';
+
+const chance = new Chance();
+
+export const UserFactory = {
+  build: (attrs: Partial<Prisma.UserCreateInput> = {}) => {
+    return {
+      email: chance.email(),
+      password: 'test1234',
+      roles: { set: [Role.USER] },
+      ...attrs,
+    };
+  },
+
+  create: async (attrs: Partial<Prisma.UserCreateInput> = {}) => {
+    const user = UserFactory.build(attrs);
+    const options: Partial<Prisma.UserCreateArgs> = {};
+    const includes = buildPrismaIncludeFromAttrs(attrs);
+    if (includes) options.include = includes;
+
+    return await prisma.user.create({
+      data: { ...user, password: hashPassword(user.password), roles: user.roles },
+      ...options,
+    });
+  },
+};
